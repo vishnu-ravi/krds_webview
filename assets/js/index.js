f6f1df7e198e9462f63894944bad635ba882fdb0
vishnu <balavishnu.r@krds.fr>
2016-02-20 00:19:47 +0530

    Initial Commit

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+var timeout;
+
+document.getElementById("btn_submit").addEventListener("click", function()
+{
+    try
+    {
+        var url	=	document.getElementById("url").value;
+
+        if(url.length == 0)
+            throw{'msg': 'Empty Url', 'elt':'#url'};
+
+        var regex = new RegExp("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
+
+        if( ! regex.test(url))
+            throw{'msg': 'Invalid Url', 'elt':'url'};
+
+        window.CallJavaAdapter.goToSite(url);
+        document.getElementById('loader').style.display	=	'block';
+        return true;
+    }
+    catch(e)
+    {
+        console.log(e.msg);
+        document.getElementById("error").innerHTML	=	e.msg;
+        document.getElementById("error").style.display	=	'block';
+
+        if(e.elt)
+            document.getElementById(e.elt).focus();
+
+        if(typeof timeout != 'undefined')
+            clearTimeout(timeout);
+
+        timeout	=	setTimeout(function()
+        {
+            document.getElementById("error").innerHTML	=	'';
+            document.getElementById("error").style.display	=	'none';
+        }, 4000);
+    }
+});
