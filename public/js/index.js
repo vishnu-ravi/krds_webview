48582b65ce527cabe8e1afb57e4d38e84ece15c5
vishnu <balavishnu.r@krds.fr>
2016-02-20 01:20:20 +0530

    node js added, css changed, select protocol field added

--- public/js/index.js
+++ public/js/index.js
@@ -4,18 +4,28 @@ document.getElementById("btn_submit").addEventListener("click", function()
 {
     try
     {
-        var url	=	document.getElementById("url").value;
+        var url     =	document.getElementById("url").value;
 
         if(url.length == 0)
-            throw{'msg': 'Empty Url', 'elt':'#url'};
+            throw{'msg': 'Empty Url', 'elt':'url'};
 
-        var regex = new RegExp("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
+        var pattern =   /^((http|https):\/\/)/;
+
+        if( ! pattern.test(url))
+        {
+            var protocol    =   document.getElementById("protocol");
+            protocol        =   protocol.options[protocol.selectedIndex].value;
+            url             =   protocol + url;
+        }
+        console.log(url);
+        var regex   =   new RegExp("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
 
         if( ! regex.test(url))
             throw{'msg': 'Invalid Url', 'elt':'url'};
 
         window.CallJavaAdapter.goToSite(url);
         document.getElementById('loader').style.display	=	'block';
+
         return true;
     }
     catch(e)
@@ -30,9 +40,9 @@ document.getElementById("btn_submit").addEventListener("click", function()
         if(typeof timeout != 'undefined')
             clearTimeout(timeout);
 
-        timeout	=	setTimeout(function()
+        timeout     =   setTimeout(function()
         {
-            document.getElementById("error").innerHTML	=	'';
+            document.getElementById("error").innerHTML      =	'';
             document.getElementById("error").style.display	=	'none';
         }, 4000);
     }
